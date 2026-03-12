86f886275299984bd5cd8125e0d0614e9334efcb
Lautaro Sabena <sabenalautaro@gmail.com>
2026-03-12 12:50:41 -0300

    Refresh autogenerated build artifacts
    Regenerate build outputs and caches: update AssemblyInformationalVersion (new build/commit identifier) across projects and refresh various obj/bin asset and AssemblyInfoInputs cache files. Also normalize ProjectDir casing in generated MSBuild editorconfig files. These are autogenerated build artifacts produced by a rebuild.

--- tests/BookingHubAPI.IntegrationTests/obj/Debug/net9.0/BookingHubAPI.IntegrationTests.AssemblyInfo.cs
+++ tests/BookingHubAPI.IntegrationTests/obj/Debug/net9.0/BookingHubAPI.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingHubAPI.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d6c47e1389dd682952aed132e3938cf5c1be924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bbf5ce55e7645a016dbc6e64e668d786b43d737")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingHubAPI.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingHubAPI.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
